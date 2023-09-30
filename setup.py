fc1462edb8213efe7c78e0b199af9a8f40f32373
ZhenyaSSSS <96760322+ZhenyaSSSS@users.noreply.github.com>
2023-09-30 17:28:47 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'model',
+  version = '0.1',
+  description = 'My ML model', 
+  py_modules = ['model'],
+  install_requires = [
+    'torch',
+    'pytorch_lightning',
+  ] 
+)
